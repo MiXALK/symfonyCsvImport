576805ce058d18ff1f6ed3bdf2d3aa246bc8bee8
M.Kantselyarchik <m.kantselyarchik@itransition.com>
2018-10-03 13:22:28 +0300

    SELF-3689 Created a mechanism which will read the CSV file, parse the contents and then insert the data into a MySQL database table. Added import rules. Created logic for display report after import.

---
+++ src/Command/CsvImportCommand.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use League\Csv\Reader;
+
+class CsvImportCommand extends Command
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    /**
+     * CsvImportCommand constructor.
+     *
+     * @param EntityManagerInterface $em
+     *
+     * @throws \Symfony\Component\Console\Exception\LogicException
+     */
+    public function __construct(EntityManagerInterface $em)
+    {
+        parent::__construct();
+
+        $this->em = $em;
+    }
+
+    /**
+     * Configure
+     * @throws \Symfony\Component\Console\Exception\InvalidArgumentException
+     */
+    protected function configure()
+    {
+        $this->setName('csv:import')
+             ->setDescription('Imports the mock CSV data file');
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     *
+     * @return void
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+        $io->title('Attempting import CSV...');
+
+        $reader = Reader::createFromPath('%kernel.root_dir%/../src/Data/stock.csv');
+
+        $results = $reader->fetchAssoc();
+
+        $resultsCounter = iterator_count($results);
+
+        $io->progressStart($resultsCounter);
+
+        $successCounter = 0;
+
+        $skippedProducts = [];
+
+        foreach ($results as $row) {
+            $product = new Product();
+            $product->setProductName($row['Product Name']);
+            $product->setProductDesc($row['Product Description']);
+            $product->setProductCode($row['Product Code']);
+            $product->setDtmAdded(new \DateTime());
+            $product->setStmTimestamp(new \DateTime());
+            $product->setStockLevel($row['Stock']);
+            $product->setPrice($row['Cost in GBP']);
+
+
+            if ($row['Discontinued'] == "yes") {
+                $product->setDtmDiscontinued(new \DateTime());
+            } else {
+                $product->setDtmDiscontinued(null);
+            }
+
+            if (($row['Cost in GBP'] > 5) and ($row['Stock'] > 10) and ($row['Cost in GBP'] < 1000)) {
+                $this->em->persist($product);
+                $successCounter = (string)++$successCounter;
+            } else {
+                $skippedProducts[] = $row['Product Code'];
+            }
+
+            $io->progressAdvance();
+        }
+
+        $skippedCounter = (string)($resultsCounter - $successCounter);
+
+        $skippedReport = implode(",", $skippedProducts);
+
+        $this->em->flush();
+
+        $io->progressFinish();
+        $io->success($successCounter.' processed successfully, '.$skippedCounter.' skipped. '."\n".'Next products skipped: '
+                    .$skippedReport.'.');
+    }
+}
